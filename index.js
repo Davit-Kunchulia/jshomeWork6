0ce5213fe78cd75e0eb07797aab8e50f9a805dbb
Davit-Kunchulia <148901201+Davit-Kunchulia@users.noreply.github.com>
2024-02-05 21:00:04 +0400

    .

--- index.js
+++ index.js
@@ -3,26 +3,44 @@ let burger = document.querySelector(".burger_menu")
 
 let menu = document.querySelector(".menu")
 
-let xMark = document.querySelector(".xmark")
+let xMark = document.querySelector(".fa-xmark")
 
 let header = document.querySelector(".header")
 
 let isOpen = false
 
+    
+
+    
 burger.addEventListener("click", () => {
     if (!isOpen) {
-        menu.classList.add("show")
-        
+    menu.classList.add("show")
+
+    burger.classList.add("hide")
+
+    xMark.style.display = "block"
+
+
+    isOpen = true
+    }
+})
+
+
+
+
+xMark.addEventListener("click", () => {
+
+    if (isOpen) {
+        menu.classList.remove("show")
+
+        xMark.style.display = "none"
+
+        burger.classList.add("show2")
         
 
-        isOpen = true
-    }
-    else {
-        menu.classList.remove("show")
-        
         isOpen = false
     }
-
 })
+
 
 
